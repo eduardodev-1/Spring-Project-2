3db3ea30040552083b780442db3cdc8e95dc070e
eduardodev-1 <122590889+eduardodev-1@users.noreply.github.com>
2023-05-30 23:35:49 -0300

    first commit

---
+++ src/main/java/com/eduardodev/application/Atividade02SpringApplication.java
@@ -0,0 +1,13 @@
+package com.eduardodev.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade02SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade02SpringApplication.class, args);
+	}
+
+}
